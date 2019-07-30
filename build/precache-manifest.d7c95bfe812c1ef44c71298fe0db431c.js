7c73acfc4e94947ecf5f1c3685f425bc43d4c02c
Jackhu <jackisplendid@gmail.com>
2019-07-30 13:34:23 +1000

    revert

---
+++ build/precache-manifest.d7c95bfe812c1ef44c71298fe0db431c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "965dfded1b9940989e51",
+    "url": "/Crime-Stats-Project/static/js/runtime~main.965dfded.js"
+  },
+  {
+    "revision": "ac2022c94b40d4c0d7f6",
+    "url": "/Crime-Stats-Project/static/js/main.ac2022c9.chunk.js"
+  },
+  {
+    "revision": "8603f9d51411e4443618",
+    "url": "/Crime-Stats-Project/static/js/1.8603f9d5.chunk.js"
+  },
+  {
+    "revision": "8603f9d51411e4443618",
+    "url": "/Crime-Stats-Project/static/css/1.f428290e.chunk.css"
+  },
+  {
+    "revision": "e05a2491069197f4d9087234199eb7c6",
+    "url": "/Crime-Stats-Project/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.d71e3530db922b5f6709318f3e1f3577.js"
+  "/Crime-Stats-Project/precache-manifest.d7c95bfe812c1ef44c71298fe0db431c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/Crime-Stats-Project/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
